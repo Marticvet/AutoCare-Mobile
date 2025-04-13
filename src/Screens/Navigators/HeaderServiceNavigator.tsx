6de838633762f7991c02e938542eb76b64d6868c
Martin Tsvetanov <martigiant@gmail.com>
2025-04-13 16:52:33 +0200

    tab container fix

--- src/Screens/Navigators/HeaderServiceNavigator.tsx
+++ src/Screens/Navigators/HeaderServiceNavigator.tsx
@@ -12,40 +12,41 @@ const Tab = createMaterialTopTabNavigator();
 
 export const HeaderServiceNavigator = () => {
     return (
-        <Tab.Navigator
-            initialRouteName={"TotalExpensesScreen"}
-            screenOptions={{
-                tabBarStyle: { backgroundColor: "#E8E8E8" },
-                tabBarActiveTintColor: "#00AFCF",
-                tabBarInactiveTintColor: "gray",
-                tabBarIndicatorStyle: { backgroundColor: "#00AFCF" },
-                tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
-            }}
-        >
-            <Tab.Screen
-                name="TotalExpensesScreen"
-                component={TotalExpensesScreen}
-                options={{ title: "Total" }}
-            />
+        <View style={{ flex: 1 }}>
+            <Tab.Navigator
+                initialRouteName={"TotalExpensesScreen"}
+                screenOptions={{
+                    tabBarStyle: { backgroundColor: "#E8E8E8" },
+                    tabBarActiveTintColor: "#00AFCF",
+                    tabBarInactiveTintColor: "gray",
+                    tabBarIndicatorStyle: { backgroundColor: "#00AFCF" },
+                    tabBarLabelStyle: { fontSize: 14, fontWeight: "bold" },
+                }}
+            >
+                <Tab.Screen
+                    name="TotalExpensesScreen"
+                    component={TotalExpensesScreen}
+                    options={{ title: "Total" }}
+                />
 
-            <Tab.Screen
-                name="FuelExpensesScreen"
-                component={FuelExpensesScreen}
-                options={{ title: "Fuel Epx" }}
-            />
-{/* 
-            <Tab.Screen
-                name="da"
-                component={FuelExpensesScreen}
-                options={{ title: "Refueling" }}
-            />
+                <Tab.Screen
+                    name="FuelExpensesScreen"
+                    component={FuelExpensesScreen}
+                    options={{ title: "Fuel Epx" }}
+                />
+                <Tab.Screen
+                    name="da"
+                    component={ReminderScreen}
+                    options={{ title: "Refueling" }}
+                />
 
-            <Tab.Screen
-                name="da1"
-                component={FuelExpensesScreen}
-                options={{ title: "Refueling" }}
-            /> */}
-        </Tab.Navigator>
+                <Tab.Screen
+                    name="da1"
+                    component={FuelExpensesScreen}
+                    options={{ title: "Refueling" }}
+                />
+            </Tab.Navigator>
+        </View>
     );
 };
 
